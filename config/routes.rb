aaac45c3c4581e466873ba5164497e5afd72420b
Trever Yarrish <trever@codingzeal.com>
2013-10-17 21:30:10 -0700

    Added route for new Attendee Info page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Roguerails::Application.routes.draw do
   root to: 'pages#show'
   # get "/:id", to: "pages#show"
   match 'dev_survey' => 'pages#dev_survey'
+  match 'attendee_info' => 'pages#attendee_info'
 end
