4132730f7333a9389186a3cd53f1b1c19df6a9a6
acuppy <acuppy@gmail.com>
2013-10-28 08:17:30 -0700

    add stay_tuned as default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Roguerails::Application.routes.draw do
-  root to: 'pages#show'
+  root to: 'pages#stay_tuned'
   # get "/:id", to: "pages#show"
   match 'dev_survey' => 'pages#dev_survey'
   match 'attendee_info' => 'pages#attendee_info'
